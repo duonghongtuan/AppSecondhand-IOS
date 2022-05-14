dc0b7c5f6c30261bdc1763cb6a4e82c99d415ac4
duonghongtuan <hongtuan2805@gmail.com>
2022-05-14 23:02:54 +0700

    Initial Commit

---
+++ SecondHandMarket/SecondHandMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondHandMarketApp.swift
+//  SecondHandMarket
+//
+//  Created by HongTuan on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SecondHandMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
